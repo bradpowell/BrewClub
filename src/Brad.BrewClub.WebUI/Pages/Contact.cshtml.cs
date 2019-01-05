d80ad365776889e55e5db40a70fbb144dec97684
bradpowell <bradpowell78@gmail.com>
2019-01-04 21:45:21 -0600

    Moved non test code folders to src folder

---
+++ src/Brad.BrewClub.WebUI/Pages/Contact.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrewClub.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Put contact info here: FB Group, possibly a main email address for the group";
+        }
+    }
+}
