07eac04234bc547d3b6639f301def545fa1ec032
bradpowell <bradpowell78@gmail.com>
2019-01-13 22:29:13 -0600

    testing CI on branch

--- tests/Brad.BrewClub.ServicesTests/IntegrationTests/Controllers/NewsItemControllerTests.cs
+++ tests/Brad.BrewClub.ServicesTests/IntegrationTests/Controllers/NewsItemControllerTests.cs
@@ -27,6 +27,8 @@ namespace Brad.BrewClub.ServicesTests.IntegrationTests.Controllers
 
             httpResponse.EnsureSuccessStatusCode();
 
+
+
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
             var newsItems = JsonConvert.DeserializeObject<IEnumerable<NewsItem>>(stringResponse);
             Assert.Contains(newsItems, newsItem => newsItem.Id == 1);
