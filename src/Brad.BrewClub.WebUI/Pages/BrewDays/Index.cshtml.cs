469a9db0181ebd97c8f5c79d13e6a3f3211688d9
Brad Powell <bradpowell78@gmail.com>
2019-01-31 21:48:21 -0600

    add back web ui src

---
+++ src/Brad.BrewClub.WebUI/Pages/BrewDays/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Brad.BrewClub.WebUI.Pages.BrewDays
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Members can post brew days and meetups";
+        }
+    }
+}
---
+++ src/Brad.BrewClub.WebUI/Pages/ClubInfo.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Brad.BrewClub.WebUI.Pages
+{
+    public class ClubInfoModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Put in description of the club.";
+        }
+    }
+}
